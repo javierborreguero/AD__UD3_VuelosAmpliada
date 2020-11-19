fb857f35086ecb7c3d2e4a29c06421bb3f73069a
javierborreguero <javiborreguero96@gmail.com>
2020-11-19 23:44:09 +0100

    Desarrollo de la opción comprar vuelos. En este commit se le piden los datos de información al usuario y se genera el codigo de venta de manera aleatoria. Los siguientes pasos son; cambiar la generacón del código al controlador y guardar los datos en el array de  vendidos.

--- ADAT_UD3_VuelosAmpliada/src/view/Test.java
+++ ADAT_UD3_VuelosAmpliada/src/view/Test.java
@@ -1,11 +1,11 @@
 package view;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
 import java.util.Map.Entry;
 
-import controller.AccesoMongo;
+import java.util.Random;
+import java.util.Scanner;
+
 import controller.Controlador;
 import model.Vuelo;
 
@@ -51,26 +51,93 @@ public class Test {
 				comprarVuelo();
 				break;
 			case 2:
+				cancelarVuelo();
 				break;
 			case 3:
+				modificarVuelo();
 				break;
 			case 4:
 				System.out.println("CERRANDO");
 				System.exit(-1);
 				break;
 			default:
+				System.err.println("ERROR: La opción introducida no es valida. Elige una opción entre 1-4");
+				elegirOperacion();
 				break;
 			}
 		} while (seleccionado);
 
 	}
 
+	/*------------------- MÉTODOS PARA COMPRAR -------------------*/
+
 	private void comprarVuelo() throws IOException {
+		sc.nextLine();
 		// Se gestiona la compra del vuelo
 		// Primero se muestran los vuelos
-		HashMap<String, Vuelo> leerVuelo = mControlador.read();
+		System.out.println("En nuestra base de datos tenemos registrados los siguientes vuelos");
+		leer();
+		// Después se pide al usuario que eliga el vuelo que quiere comprar en función
+		// del codigo de vuelo;
+		System.out.print("Escribe el codigo del vuelo que quieres comprar ");
+		String elegirCodigoVuelo = sc.nextLine();
+
+		for (Entry<String, Vuelo> entry : mControlador.read().entrySet()) {
+			if (elegirCodigoVuelo.equalsIgnoreCase(entry.getValue().getCodigo_vuelo())) {
+				System.out.println("Has elegido el vuelo " + entry.getValue().getCodigo_vuelo() + " con origen en "
+						+ entry.getValue().getOrigen() + " y destino en " + entry.getValue().getDestino() + "\n");
+				guardarDatos();
+			}
+
+		}
+
+	}
+
+	private void guardarDatos() {
+
+		// El siguiente paso es pedir los datos del comprador (nombre, dni etc.)
+		System.out.println("Para poder efectuar la compra primero necesitamos sus datos.\n");
+		System.out.print("Escribe tu nombre ");
+		String nombre = sc.nextLine();
+		System.out.print("Escribe tu primer apellido ");
+		String apellido = sc.nextLine();
+		System.out.print("Escribe tu DNI ");
+		String dni = sc.nextLine();
+		System.out.print("Escribe el DNI de la persona que va a pagar el vuelo ");
+		String dniPagador = sc.nextLine();
+		System.out.print("Por último, indique el número de tarjeta con la que se va a pagar ");
+		String numeroTarjeta = sc.nextLine();
+		if (!nombre.equals("") && !apellido.equals("") && !dni.equals("") && !dniPagador.equals("")
+				&& !numeroTarjeta.equals("")) {
+
+			/*----- Se gener un codigo de venta aleatorio (esto es de prueba, hay que pasarlo al controlador) -----*/
+			char[] cadenaCaracteres = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+			StringBuilder sbPrimeraParte = new StringBuilder();
+			StringBuilder sbSegundaParte = new StringBuilder();
+			Random random = new Random();
+			int numero;
+			String cadenaPrincipal = ""; // Inicializamos la Variable/
+			for (int i = 0; i < 2; i++) {
+				char letrasPrimeraParte = cadenaCaracteres[random.nextInt(cadenaCaracteres.length)];
+				char letrasSegundaParte = cadenaCaracteres[random.nextInt(cadenaCaracteres.length)];
+				sbPrimeraParte.append(letrasPrimeraParte);
+				sbSegundaParte.append(letrasSegundaParte);
+				numero = (int) (random.nextDouble() * 99 + 1000);
+				cadenaPrincipal = sbPrimeraParte.toString() + numero + sbSegundaParte.toString();
+			}
+			String codigoVuelo = cadenaPrincipal.toUpperCase();
+			System.out.println("Se ha generado el siguiente codigo de vuelo " + codigoVuelo);
+
+			// TODO INSERTAR CAMBIOD EN EL ARRAY VENDIDOS
+			/*-----------------------------------------------------------------------------------------------------*/
+		} else {
+			System.err.println("ERROR: Todos los datos deben estar completos");
+		}
+	}
+
+	private void leer() throws IOException {
 		int contador = 1;
-		for (Entry<String, Vuelo> entry : leerVuelo.entrySet()) {
+		for (Entry<String, Vuelo> entry : mControlador.read().entrySet()) {
 			System.out.println("<----- Vuelo " + contador + " ----->");
 			System.out.println("Codigo del vuelo: " + entry.getValue().getCodigo_vuelo());
 			System.out.println("Origen: " + entry.getValue().getOrigen());
@@ -82,6 +149,18 @@ public class Test {
 			System.out.println("-----------------------\n");
 			contador++;
 		}
+	}
+	/*--------------------------------------*/
+
+	/*------------------- MÉTODOS PARA CANCELAR/BORRAR -------------------*/
+	private void cancelarVuelo() {
+		// TODO En este método se trabajará la opción de cancelar un vuelo
+
+	}
+
+	/*------------------- MÉTODOS PARA MODIFICAR -------------------*/
+	private void modificarVuelo() {
+		// TODO Auto-generated method stub
 
 	}
 }
